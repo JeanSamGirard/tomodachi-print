dbfc721f641c5639b0b9867faef2e87a6c6f2d5a
JeanSamGirard <jeansam@live.ca>
2026-04-23 21:09:44 -0400

    reduce wait

--- utils/macro/wait.js
+++ utils/macro/wait.js
@@ -2,5 +2,5 @@
 // Currently this is required to prevent the game dropping inputs
 // It does significantly slow down printing tho so if I find another solution that would be nice
 export const macroWait = () => {
-  return "0.5s\n";
+  return "0.25s\n";
 };
