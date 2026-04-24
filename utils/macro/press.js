215745c1aa53c5370dee31edeede1e7650e39c16
JeanSamGirard <jeansam@live.ca>
2026-04-23 21:10:52 -0400

    reduce press time

--- utils/macro/press.js
+++ utils/macro/press.js
@@ -1,6 +1,6 @@
 import { macroWait } from "./wait.js";
 
 // Return a macro that press a button
-export const macroPress = (button, duration = "0.1s") => {
+export const macroPress = (button, duration = "0.08s") => {
   return button + " " + duration + "\n";
 };
